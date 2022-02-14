fcc30b16fe9c1c587cd1e1116da674bfce81fb38
Connor Kerrigan <ac111484@avcol.school.nz>
2022-02-14 17:51:16 +1300

    Revert "the first commit wahooo"
    This reverts commit 8d2dd83342533b018eebd030c8a3ad2c7a1ec9c7.

--- MvcMovie/Models/Movies.cs
+++ MvcMovie/Models/Movies.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
@@ -9,12 +7,9 @@ namespace MvcMovie.Models
         public int Id { get; set; }
         public string? Title { get; set; }
 
-        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
-
-        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
